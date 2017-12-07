c93f473ddb7a4494048da616e902c84a0089fa34
Collin <c_orear@icloud.com>
2017-12-07 22:11:47 +0000

    k

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,15 @@
 class Message < ActiveRecord::Base
   belongs_to :user
-  has_attached_file :image,
-  :storage => :cloudinary,
-  :path => ':id/:style/:filename',
-  :styles => {thumb: "100x100#"}
-    validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
-    
+  
   validates :user_id, presence: true
   validates :content, presence: true
   default_scope -> { order(created_at: :desc) }
+  
+  has_attached_file :picture,
+  :storage => :cloudinary,
+  :path => ':id/:style/:filename',
+  :styles => {thumb: "100x100#"}
+  
+  validates_attachment_content_type :picture, content_type: /\Aimage\/.*\z/
+  
 end
